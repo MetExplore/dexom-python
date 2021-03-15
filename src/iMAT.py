c9ef0a3c785c3fa018c46167e7008a0267a830f1
Maximilian Stingl <maximilian.stingl@inrae.fr>
2021-03-15 15:18:23 +0100

    PEP8

--- src/iMAT.py
+++ src/iMAT.py
@@ -49,9 +49,9 @@ def imat(model, reaction_weights=None, epsilon=1., threshold=1e-1, *args, **kwar
                 xr_upper = model.solver.interface.Constraint(
                     rxn.reverse_variable + rxn.lower_bound * xr, ub=0., name="xf_%s_upper" % rid)
                 xf_lower = model.solver.interface.Constraint(
-                    rxn.forward_variable - 2*threshold * xf, lb=0., name="xf_%s_lower" % rid)
+                    rxn.forward_variable - threshold * xf, lb=0., name="xf_%s_lower" % rid)
                 xr_lower = model.solver.interface.Constraint(
-                    rxn.reverse_variable - 2*threshold * xr, lb=0., name="xr_%s_lower" % rid)
+                    rxn.reverse_variable - threshold * xr, lb=0., name="xr_%s_lower" % rid)
                 model.solver.add(xtot_def)
                 model.solver.add(xf_upper)
                 model.solver.add(xr_upper)
