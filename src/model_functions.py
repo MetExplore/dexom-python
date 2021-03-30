1bc8820678167f14c0411811f14bb81eca3f2ca6
Maximilian Stingl <maximilian.stingl@inrae.fr>
2021-03-30 16:44:14 +0200

    started working on create_weights_from_gpr function

--- src/model_functions.py
+++ src/model_functions.py
@@ -1,6 +1,10 @@
 
 import six
+import pandas as pd
 from csv import DictReader, DictWriter
+from cobra.io import load_json_model, read_sbml_model
+from sympy import Add, Mul, sympify
+from sympy.functions.elementary.miscellaneous import Max, Min
 
 
 def clean_model(model, reaction_weights=None, full=False):
@@ -63,3 +67,33 @@ def load_reaction_weights(filename):
         reaction_weights[k] = float(v)
 
     return reaction_weights
+
+
+def create_weights_from_gpr(model, gene_file):
+    reaction_weights = {}
+
+    genes = pd.read_csv(gene_file, sep=";")
+    gene_weights = pd.DataFrame(genes["t"])
+    gene_weights.index = genes["entrez"]
+    gene_weights = gene_weights["t"].to_dict()
+    gene_weights = {"HGNC:"+str(k): float(v) for k, v in gene_weights.items()}
+
+    for rxn in model.reactions:
+        if len(rxn.genes) > 0 and any([g.id in gene_weights.keys() for g in rxn.genes]):
+            expression = rxn.gene_expression()  # sympify(rxn.gene_reaction_rule)
+            expression = expression.replace(Mul, Max).replace(Add, Min)
+            gene_dict = {g.id: gene_weights.get(g.id) for g in rxn.genes}
+            reaction_weights[rxn.id] = expression.subs(gene_dict).evalf()
+
+    return gene_weights, reaction_weights
+
+
+if __name__ == "__main__":
+
+    jul_model = read_sbml_model("recon2_2/Recon2.2_reimported2.xml")
+    # old_model = read_sbml_model("recon2_2/Recon2.2_Swainton2016.xml")
+    filename = "recon2_2/recon2.2_tvals.csv"
+    gen_wei, rec_wei = create_weights_from_gpr(jul_model, filename)
+    print(gen_wei)
+    print("------------------------")
+    print(rec_wei)
