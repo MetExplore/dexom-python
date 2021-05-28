7ac2bcf8d6a88948350fdc52dbaedd10210491e8
Maximilian Stingl <maximilian.stingl@inrae.fr>
2021-05-28 18:29:23 +0200

    implementing approach2

--- src/enum_functions/diversity_enum.py
+++ src/enum_functions/diversity_enum.py
@@ -9,13 +9,13 @@ from cobra.io import load_json_model, load_matlab_model, read_sbml_model
 from src.imat import imat, create_partial_variables, create_full_variables
 from src.result_functions import read_solution, get_binary_sol, write_solution
 from src.model_functions import load_reaction_weights
-from enumeration import EnumSolution, dexom_results
+from enumeration import EnumSolution, get_recent_solution_and_iteration
 from icut import create_icut_constraint
 from maxdist import create_maxdist_constraint, create_maxdist_objective
 
 
 def diversity_enum(model, reaction_weights, prev_sol, thr=1e-5, eps=1e-2, obj_tol=1e-3, maxiter=10, dist_anneal=0.995,
-                   out_path="enum_dexom", icut=True, only_ones=False, full=False):
+                   out_path="enum_dexom", icut=True, full=False, save=False):
     """
     diversity-based enumeration
     Parameters
@@ -82,12 +82,12 @@ def diversity_enum(model, reaction_weights, prev_sol, thr=1e-5, eps=1e-2, obj_to
         i = 0
         for rid, weight in six.iteritems(reaction_weights):
             rid_loc = prev_sol.fluxes.index.get_loc(rid)
-            if (prev_sol_bin[rid_loc] == 1 or not only_ones) and np.random.random() > dist_anneal**idx:
+            if np.random.random() > dist_anneal**idx:
                 tempweights[rid] = weight
                 i += 1
         selected_recs.append(i)
 
-        objective = create_maxdist_objective(model, tempweights, prev_sol, prev_sol_bin, only_ones, full)
+        objective = create_maxdist_objective(model, tempweights, prev_sol, prev_sol_bin, full=full)
         model.objective = objective
         try:
             t2 = time.perf_counter()
@@ -97,11 +97,14 @@ def diversity_enum(model, reaction_weights, prev_sol, thr=1e-5, eps=1e-2, obj_to
             prev_sol_bin = get_binary_sol(prev_sol, thr)
             all_solutions.append(prev_sol)
             all_binary.append(prev_sol_bin)
+            if save:
+                write_solution(prev_sol, thr, filename=out_path+"_solution_"+time.strftime("%Y%m%d-%H%M%S")+".csv")
             t1 = time.perf_counter()
             print("time for optimizing in iteration " + str(idx) + ":", t1 - t2)
             times.append(t1 - t0)
         except:
             print("An error occured in iteration %i of dexom, check if all feasible solutions have been found" % idx)
+            times.append(0.)
             break
 
     model.solver.remove([const for const in icut_constraints if const in model.solver.constraints])
@@ -116,105 +119,118 @@ def diversity_enum(model, reaction_weights, prev_sol, thr=1e-5, eps=1e-2, obj_to
 
     return solution
 
-#
-# if __name__ == "__main__":
-#     description = "Performs the reaction enumeration algorithm on a specified list of reactions"
-#
-#     parser = argparse.ArgumentParser(description=description, formatter_class=argparse.RawTextHelpFormatter)
-#     parser.add_argument("-m", "--model", help="Metabolic model in sbml, matlab, or json format")
-#     parser.add_argument("-r", "--reaction_weights", default=None,
-#                         help="Reaction weights in csv format (first row: reaction names, second row: weights)")
-#     parser.add_argument("-p", "--prev_sol", default=None, help="initial imat solution in .txt format")
-#     parser.add_argument("--epsilon", type=float, default=1e-2,
-#                         help="Activation threshold for highly expressed reactions")
-#     parser.add_argument("--threshold", type=float, default=1e-5, help="Activation threshold for all reactions")
-#     parser.add_argument("-t", "--timelimit", type=int, default=None, help="Solver time limit")
-#     parser.add_argument("-i", "--maxiter", type=int, default=10, help="Iteration limit")
-#     parser.add_argument("--tol", type=float, default=1e-6, help="Solver feasibility tolerance")
-#     parser.add_argument("--mipgap", type=float, default=1e-3, help="Solver MIP gap tolerance")
-#     parser.add_argument("--obj_tol", type=float, default=1e-3, help="objective function tolerance")
-#     parser.add_argument("-o", "--output", default="div_enum", help="Base name of output files, without format")
-#     parser.add_argument("-a", "--dist_anneal", type=float, default=0.995, help="annealing distance")
-#     parser.add_argument("--noicut", action='store_true', help="Use this flag to remove the icut constraint")
-#     parser.add_argument("--full", action='store_true', help="")
-#     args = parser.parse_args()
-#
-#     fileformat = Path(args.model).suffix
-#     if fileformat == ".sbml" or fileformat == ".xml":
-#         model = read_sbml_model(args.model)
-#     elif fileformat == '.json':
-#         model = load_json_model(args.model)
-#     elif fileformat == ".mat":
-#         model = load_matlab_model(args.model)
-#     else:
-#         print("Only SBML, JSON, and Matlab formats are supported for the models")
-#         model = None
-#
-#     try:
-#         model.solver = 'cplex'
-#     except:
-#         print("cplex is not available or not properly installed")
-#
-#     reaction_weights = {}
-#     if args.reaction_weights:
-#         reaction_weights = load_reaction_weights(args.reaction_weights)
-#
-#     if args.initial_solution:
-#         prev_sol, prev_bin = read_solution(args.initial_solution)
-#     else:
-#         prev_sol = imat(model, reaction_weights, epsilon=args.epsilon, threshold=args.threshold,
-#                                 timelimit=args.timelimit, feasibility=args.tol, mipgaptol=args.mipgap)
-#
-#     icut = False
-#     if args.noicut:
-#       icut = True
-#
-#     if args.full:
-#         for rxn in model.reactions:
-#             if rxn.id not in reaction_weights:
-#                 reaction_weights[rxn.id] = args.obj_tol*1e-5
-#             elif reaction_weights[rxn.id] == 0:
-#                 reaction_weights[rxn.id] = args.obj_tol*1e-5
-#
-#     dexom_sol = diversity_enum(model=model, reaction_weights=reaction_weights, prev_sol=prev_sol, thr=args.threshold,
-#                                maxiter=args.maxiter, obj_tol=args.obj_tol, dist_anneal=args.dist_anneal, icut=icut,
-#                                out_path=args.output, only_ones=False, full=False)
 
 if __name__ == "__main__":
-    # write_batch_script(100)
+    description = "Performs the reaction enumeration algorithm on a specified list of reactions"
 
-    from cobra.io import load_json_model, read_sbml_model, load_matlab_model
-    from src.model_functions import load_reaction_weights
+    parser = argparse.ArgumentParser(description=description, formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument("-m", "--model", help="Metabolic model in sbml, matlab, or json format")
+    parser.add_argument("-r", "--reaction_weights", default=None,
+                        help="Reaction weights in csv format (first row: reaction names, second row: weights)")
+    parser.add_argument("-p", "--prev_sol", default=None, help="starting solution or directory of recent solutions")
+    parser.add_argument("--epsilon", type=float, default=1e-2,
+                        help="Activation threshold for highly expressed reactions")
+    parser.add_argument("--threshold", type=float, default=1e-5, help="Activation threshold for all reactions")
+    parser.add_argument("-t", "--timelimit", type=int, default=None, help="Solver time limit")
+    parser.add_argument("-i", "--maxiter", type=int, default=10, help="Iteration limit")
+    parser.add_argument("--tol", type=float, default=1e-6, help="Solver feasibility tolerance")
+    parser.add_argument("--mipgap", type=float, default=1e-3, help="Solver MIP gap tolerance")
+    parser.add_argument("--obj_tol", type=float, default=1e-3, help="objective function tolerance")
+    parser.add_argument("-o", "--output", default="div_enum", help="Base name of output files, without format")
+    parser.add_argument("-a", "--dist_anneal", type=float, default=0.995, help="annealing distance")
+    parser.add_argument("-s", "--startsol_num", type=int, default=100, help="number of starting solutions")
+    parser.add_argument("--noicut", action='store_true', help="Use this flag to remove the icut constraint")
+    parser.add_argument("--full", action='store_true', help="Use this flag to assign non-zero weights to all reactions")
+    parser.add_argument("--save", action='store_true', help="Use this flag to save each individual solution")
+    args = parser.parse_args()
 
-    model = read_sbml_model("min_iMM1865/min_iMM1865.xml")
-    reaction_weights = load_reaction_weights("min_iMM1865/p53_deseq2_cutoff_padj_1e-6.csv")
-
-    # model = load_matlab_model("recon2_2/recon2v2_corrected.mat")
-    # reaction_weights = load_reaction_weights("recon2_2/microarray_reactions_test.csv", "reactions", "scores")
+    fileformat = Path(args.model).suffix
+    if fileformat == ".sbml" or fileformat == ".xml":
+        model = read_sbml_model(args.model)
+    elif fileformat == '.json':
+        model = load_json_model(args.model)
+    elif fileformat == ".mat":
+        model = load_matlab_model(args.model)
+    else:
+        print("Only SBML, JSON, and Matlab formats are supported for the models")
+        model = None
 
     try:
         model.solver = 'cplex'
     except:
         print("cplex is not available or not properly installed")
 
+    reaction_weights = {}
+    if args.reaction_weights:
+        reaction_weights = load_reaction_weights(args.reaction_weights)
+
+    a = args.dist_anneal
+    if "." in args.prev_sol:
+        prev_sol, prev_bin = read_solution(args.prev_sol)
+        model = create_partial_variables(model, reaction_weights, epsilon=args.epsilon)
+    elif args.prev_sol:
+        prev_sol, i = get_recent_solution_and_iteration(args.prev_sol)
+        a = a ** i
+        model = create_partial_variables(model, reaction_weights, epsilon=args.epsilon)
+    else:
+        prev_sol = imat(model, reaction_weights, epsilon=args.epsilon, threshold=args.threshold,
+                        timelimit=args.timelimit, feasibility=args.tol, mipgaptol=args.mipgap)
+
     icut = False
-    full = False
-    only_ones = False
+    if args.noicut:
+      icut = True
 
-    for rxn in model.reactions:
-        if rxn.id not in reaction_weights:
-            reaction_weights[rxn.id] = -1e-8
-        elif reaction_weights[rxn.id] == 0:
-            reaction_weights[rxn.id] = -1e-8
+    save = False
+    if args.save:
+        save = True
 
-    model.solver.configuration.verbosity = 2
-    imat_solution = imat(model, reaction_weights, feasibility=1e-7, timelimit=6000, full=full)
+    if args.full:
+        for rxn in model.reactions:
+            if rxn.id not in reaction_weights:
+                reaction_weights[rxn.id] = args.obj_tol*1e-5
+            elif reaction_weights[rxn.id] == 0:
+                reaction_weights[rxn.id] = args.obj_tol*1e-5
 
-    print("\nstarting dexom")
-    dexom_sol = diversity_enum(model, reaction_weights, imat_solution, maxiter=10, obj_tol=1e-3, dist_anneal=0.995,
-                               icut=icut, only_ones=only_ones, full=full)
-    print("\n")
+    dexom_sol = diversity_enum(model=model, reaction_weights=reaction_weights, prev_sol=prev_sol, thr=args.threshold,
+                               maxiter=args.maxiter, obj_tol=args.obj_tol, dist_anneal=a, icut=icut,
+                               out_path=args.output, full=False, save=save)
 
-    ## dexom result analysis
 
-    solutions = dexom_results("enum_dexom_results.csv", "enum_dexom_solutions.csv", "enum_dexom_newicut")
+# if __name__ == "__main__":
+#     # write_batch_script(100)
+#
+#     from cobra.io import load_json_model, read_sbml_model, load_matlab_model
+#     from src.model_functions import load_reaction_weights
+#
+#     model = read_sbml_model("min_iMM1865/min_iMM1865.xml")
+#     reaction_weights = load_reaction_weights("min_iMM1865/p53_deseq2_cutoff_padj_1e-6.csv")
+#
+#     # model = load_matlab_model("recon2_2/recon2v2_corrected.mat")
+#     # reaction_weights = load_reaction_weights("recon2_2/microarray_reactions_test.csv", "reactions", "scores")
+#
+#     try:
+#         model.solver = 'cplex'
+#     except:
+#         print("cplex is not available or not properly installed")
+#
+#     icut = False
+#     full = False
+#     only_ones = False
+#
+#     for rxn in model.reactions:
+#         if rxn.id not in reaction_weights:
+#             reaction_weights[rxn.id] = -1e-8
+#         elif reaction_weights[rxn.id] == 0:
+#             reaction_weights[rxn.id] = -1e-8
+#
+#     model.solver.configuration.verbosity = 2
+#     imat_solution = imat(model, reaction_weights, feasibility=1e-6, timelimit=6000, full=full)
+#
+#     print("\nstarting dexom")
+#     dexom_sol = diversity_enum(model, reaction_weights, imat_solution, maxiter=10, obj_tol=1e-3, dist_anneal=0.999,
+#                                icut=icut, only_ones=only_ones, full=full)
+#     print("\n")
+#
+#     ## dexom result analysis
+#
+#     solutions = dexom_results("enum_dexom_results.csv", "enum_dexom_solutions.csv", "enum_dexom")
--- src/enum_functions/enumeration.py
+++ src/enum_functions/enumeration.py
@@ -1,8 +1,12 @@
 
+import os
+from pathlib import Path
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from src.imat import imat
+from src.model_functions import load_reaction_weights
+from src.result_functions import read_solution
 
 
 class EnumSolution(object):
@@ -12,22 +16,58 @@ class EnumSolution(object):
         self.objective_value = objective_value
 
 
-def write_batch_script(filenums):
+def get_recent_solution_and_iteration(dirpath, startsol_num):
+    paths = sorted(list(Path(dirpath).glob("*solution_*.csv")), key=os.path.getctime)
+    paths.reverse()
+    solpath = paths[int(np.random.exponential(5))]
+    solution, binary = read_solution(solpath)
+    iteration = len(paths) + 1 - startsol_num
+    return solution, iteration
+
+
+def write_batch_script1(filenums):
     for i in range(filenums):
-        with open("BATCH/file_"+str(i)+".sh", "w+") as file:
-            file.write('#!/bin/bash\n#SBATCH -p workq\n#SBATCH --mail-type=ALL\n#SBATCH --mem=64G\n#SBATCH -c 24\n'
-                       '#SBATCH -t 00:10:00\n#SBATCH -J rxn_enum_%i\n#SBATCH -o rxnout%i.out\n#SBATCH -e rxnerr%i.out\n'
-                       % (i, i, i))
-            file.write('cd /home/mstingl/work/dexom_py\nmodule purge\nmodule load system/Python-3.7.4\nsource env/bin/'
-                       'activate\nexport PYTHONPATH=${PYTHONPATH}:"/home/mstingl/work/CPLEX_Studio1210/cplex/python/3.7'
-                       '/x86-64_linux"\npython src/enum_functions/rxn_enum.py -o parallel_approach1/rxn_enum_%i --range'
-                       ' %i_%i -m min_iMM1865/min_iMM1865.xml -r min_iMM1865/p53_deseq2_cutoff_padj_1e-6.csv -l '
-                       'min_iMM1865/min_iMM1865_reactions_shuffled.txt -t 600\n' % (i, i*5, i*5+5))
-            a = 0.999**i
-            file.write('python src/enum_functions/diversity_enum.py -o parallel_approach1/div_enum_%i -m '
-                       'min_iMM1865/min_iMM1865.xml -r min_iMM1865/p53_deseq2_cutoff_padj_1e-6.csv -p '
-                       'parallel_approach1/rxn_enum_%i_solution_0.csv -a %.5f' % (i, i, a))
-    return 0
+        with open("parallel_approach1/file_"+str(i)+".sh", "w+") as f:
+            f.write('#!/bin/bash\n#SBATCH -p workq\n#SBATCH --mail-type=ALL\n#SBATCH --mem=64G\n#SBATCH -c 24\n'
+                    '#SBATCH -t 02:00:00\n#SBATCH -J dexom1_%i\n#SBATCH -o dex1out%i.out\n#SBATCH -e dex1err%i.out\n'
+                    % (i, i, i))
+            f.write('cd /home/mstingl/work/dexom_py\nmodule purge\nmodule load system/Python-3.7.4\nsource env/bin/'
+                    'activate\nexport PYTHONPATH=${PYTHONPATH}:"/home/mstingl/work/CPLEX_Studio1210/cplex/python/3.7'
+                    '/x86-64_linux"\n')
+            f.write('python src/enum_functions/rxn_enum.py -o parallel_approach1/rxn_enum_%i --range %i_%i '
+                    '-m min_iMM1865/min_iMM1865.xml -r min_iMM1865/p53_deseq2_cutoff_padj_1e-6.csv '
+                    '-l min_iMM1865/min_iMM1865_reactions_shuffled.txt -t 600\n' % (i, i*5, i*5+10))
+            a = (1-1/(filenums*2))**i
+            f.write('python src/enum_functions/diversity_enum.py -o parallel_approach1/div_enum_%i -m '
+                    'min_iMM1865/min_iMM1865.xml -r min_iMM1865/p53_deseq2_cutoff_padj_1e-6.csv -p '
+                    'parallel_approach1/rxn_enum_%i_solution_0.csv -a %.5f' % (i, i, a))
+    return True
+
+
+def write_batch_script2(filenums):
+    paths = sorted(list(Path("parallel_approach2").glob("*solution_*.csv")), key=os.path.getctime)
+    paths.reverse()
+    for i in range(filenums):
+        with open("parallel_approach2/rxnstart_"+str(i)+".sh", "w+") as f:
+            f.write('#!/bin/bash\n#SBATCH -p workq\n#SBATCH --mail-type=ALL\n#SBATCH --mem=64G\n#SBATCH -c 24\n'
+                    '#SBATCH -t 00:05:00\n#SBATCH -J dexom2_%i\n#SBATCH -o dex2out%i.out\n#SBATCH -e dex2err%i.out\n'
+                    % (i, i, i))
+            f.write('cd /home/mstingl/work/dexom_py\nmodule purge\nmodule load system/Python-3.7.4\nsource env/bin/'
+                    'activate\nexport PYTHONPATH=${PYTHONPATH}:"/home/mstingl/work/CPLEX_Studio1210/cplex/python/3.7'
+                    '/x86-64_linux"\n')
+            sol = paths[i]
+            f.write('python src/enum_functions/diversity_enum.py -o parallel_approach2/div_enum_%i_0 -m '
+                    'min_iMM1865/min_iMM1865.xml -r min_iMM1865/p53_deseq2_cutoff_padj_1e-6.csv -p %s -i 1 -a 0.995 '
+                    '--save' % (i, sol))
+    with open("parallel_approach2/dexomstart.sh", "w+") as f:
+        f.write('#!/bin/bash\n#SBATCH -p workq\n#SBATCH --mail-type=ALL\n#SBATCH --mem=64G\n#SBATCH -c 24\n'
+                '#SBATCH -t 00:10:00\n')
+        f.write('cd /home/mstingl/work/dexom_py\nmodule purge\nmodule load system/Python-3.7.4\nsource env/bin/'
+                'activate\nexport PYTHONPATH=${PYTHONPATH}:"/home/mstingl/work/CPLEX_Studio1210/cplex/python/3.7'
+                '/x86-64_linux"\n')
+        f.write('python src/enum_functions/diversity_enum.py -o parallel_approach2/div_enum -m '
+                'min_iMM1865/min_iMM1865.xml -r min_iMM1865/p53_deseq2_cutoff_padj_1e-6.csv -p parallel_approach2 '
+                '-i 1 -a 0.995 --save')
 
 
 def dexom_results(result_path, solution_path, out_path):
@@ -77,5 +117,86 @@ def dexom_results(result_path, solution_path, out_path):
     return df.T
 
 
+def dexom_cluster_results(in_folder, out_folder):
+
+    # concatenating all .out files from the cluster
+    with open(out_folder+'/all_outs.txt', 'w+') as outfile:
+        for i in range(100):
+            fname = in_folder+'/rxnout'+str(i)+'.out'
+            with open(fname) as infile:
+                outfile.write(infile.read())
+    with open(out_folder+'/all_errs.txt', 'w+') as outfile:
+        for i in range(100):
+            fname = in_folder+'/rxnerr'+str(i)+'.out'
+            with open(fname) as infile:
+                outfile.write(infile.read())
+
+    #concatenating & analyzing rxn_enum results
+    print("looking at rxn_enum")
+    all_rxn = []
+    for i in range(100):
+        try:
+            rxn = pd.read_csv(in_folder+'/rxn_enum_%i_solutions.csv' % i, index_col=0)
+            all_rxn.append(rxn)
+        except:
+            pass
+    rxn = pd.concat(all_rxn, ignore_index=True)
+    rxn.to_csv(out_folder+"/all_rxn.csv")
+    unique = len(rxn.drop_duplicates())
+    print("There are %i unique solutions and %i duplicates" % (unique, len(rxn)-unique))
+    fulltime = 0
+    counter = 0
+    with open(out_folder+"/all_outs.txt", "r") as file:
+        for line in file:
+            line = line.split()
+            try:
+                fulltime += float(line[0])
+                counter += 1
+            except:
+                pass
+    print("Total computation time:", fulltime)
+    print("Average time per iteration:", fulltime*2/counter)
+
+    # concatenating & analyzing diversity_enum results
+    print("looking at diversity_enum")
+    all_res = []
+    all_sol = []
+    for i in range(100):
+        try:
+            sol = pd.read_csv(in_folder+'/div_enum_%i_solutions.csv' % i, index_col=0)
+            res = pd.read_csv(in_folder+'/div_enum_%i_results.csv' % i, index_col=0)
+            all_sol.append(sol)
+            all_res.append(res)
+        except:
+            pass
+    sol = pd.concat(all_sol, ignore_index=True)
+    res = pd.concat(all_res, ignore_index=True)
+    sol.to_csv(out_folder+"/all_sol.csv")
+    res.to_csv(out_folder+"/all_res.csv")
+    dexom_results(out_folder+"/all_res.csv", out_folder+"/all_sol.csv", out_folder+"/all_dexom")
+
+    plt.clf()
+    fig = res.sort_values("selected reactions").reset_index(drop=True)["selected reactions"].plot().get_figure()
+    fig.savefig(out_folder+"/all_dexom_selected_reactions_ordered.png")
+
+    # analyzing total results
+    print("total result")
+    full = pd.concat([rxn, sol], ignore_index=True)
+    unique = len(full.drop_duplicates())
+    print("There are %i unique solutions and %i duplicates" % (unique, len(full)-unique))
+
+    return full.drop_duplicates()
+
+
 if __name__ == "__main__":
-    write_batch_script(100)
+
+    write_batch_script2(100)
+
+    # sol = dexom_cluster_results("parallel_approach1", "parallel_approach1_analysis")
+    #
+    # # calculating objective values
+    # recs = load_reaction_weights("min_iMM1865/p53_deseq2_cutoff_padj_1e-6.csv")
+    # imsol = read_solution("parallel_approach1/rxn_enum_0_solution_0.csv")
+    # weights = np.array([recs[key] if key in recs else 0 for key in imsol[0].fluxes.index])
+    # obj = [np.dot(np.array(s[1]), weights) for s in sol.iterrows()]
+    # print("objective value range: ", min(obj), ",", max(obj))
