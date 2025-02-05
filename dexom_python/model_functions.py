5e2dba63be366ccf8ebcd933dd696d1296031447
mstingl <maximilian.stingl@inrae.fr>
2025-02-05 16:00:51 +0100

    add check_model_primals

--- dexom_python/model_functions.py
+++ dexom_python/model_functions.py
@@ -35,10 +35,10 @@ def read_model(modelfile, solver='cplex'):
     return model
 
 
-def check_model_options(model, timelimit=DEFAULT_VALUES['timelimit'], feasibility=DEFAULT_VALUES['tolerance'],
+def check_model_options(model, timelimit=DEFAULT_VALUES['timelimit'], tolerance=DEFAULT_VALUES['tolerance'],
                         mipgaptol=DEFAULT_VALUES['mipgap'], verbosity=DEFAULT_VALUES['verbosity']):
     model.solver.configuration.timeout = timelimit
-    model.tolerance = feasibility
+    model.tolerance = tolerance
     model.solver.configuration.verbosity = verbosity
     model.solver.configuration.presolve = True
     if hasattr(optlang, 'cplex_interface'):
@@ -51,32 +51,39 @@ def check_model_options(model, timelimit=DEFAULT_VALUES['timelimit'], feasibilit
 
 def check_threshold_tolerance(model, epsilon, threshold):
     cobra_config = cobra.Configuration()
-    limit = model.tolerance * max(abs(cobra_config.upper_bound), abs(cobra_config.lower_bound))
-    if threshold < 2*limit:
-        raise ValueError('The threshold parameter value is too low compared to the current model tolerance. '
-                         'Current threshold value: %s. Current tolerance value:%s. Minimum threshold value: %s'
-                         % (str(threshold), str(model.tolerance), str(2*limit)))
-    if epsilon < np.around(threshold + limit, 10):
-        raise ValueError('The epsilon parameter value is too low compared to the current threshold and model tolerance.'
-                         ' Current epsilon value: %s. Current threshold value: %s. Current tolerance value:%s. '
-                         'Minimum epsilon value: %s'
-                         % (str(epsilon), str(threshold), str(model.tolerance), str(threshold + limit)))
-    return 0
+    # limit = model.tolerance * max(abs(cobra_config.upper_bound), abs(cobra_config.lower_bound))
+    limit = model.solver.configuration.tolerances.integrality * np.max([r.bounds for r in model.reactions]) + model.solver.configuration.tolerances.feasibility
+    if threshold < limit:
+        UserWarning(f'The threshold parameter is below the detection limit for RL reactions. RL reactions can only have guaranteed flux < {limit}')
+    limit = (limit + 1e-6) / (1 - limit - model.solver.configuration.tolerances.feasibility)
+    if epsilon < limit:
+        UserWarning(f'The epsilon parameter value is too low compared the detection limit for RH reactions. RH reactions can only have guaranteed flux > {limit}')
+        epsilon = limit
+
+    # if threshold < 2*limit:
+    #     raise ValueError('The threshold parameter value is too low compared to the current model tolerance. '
+    #                      'Current threshold value: %s. Current tolerance value:%s. Minimum threshold value: %s'
+    #                      % (str(threshold), str(model.tolerance), str(2*limit)))
+    # if epsilon < np.around(threshold + limit, 10):
+    #     raise ValueError('The epsilon parameter value is too low compared to the current threshold and model tolerance.'
+    #                      ' Current epsilon value: %s. Current threshold value: %s. Current tolerance value:%s. '
+    #                      'Minimum epsilon value: %s'
+    #                      % (str(epsilon), str(threshold), str(model.tolerance), str(threshold + limit)))
+    return epsilon, threshold
 
 
 def check_constraint_primal_values(model):
     for c in model.constraints:
+        error = False
         if c.ub is None:
             if c.primal < c.lb - model.tolerance:
-                print('Invalid constraint value for %s: (lb, primal, ub) = (%s, %s, %s)'
-                      % (c.name, str(c.lb), str(c.primal), str(c.ub)))
-                raise OptimizationError('Invalid constraint value for %s ' % c.name)
+                error = True
         elif c.lb is None:
             if c.primal > c.ub + model.tolerance:
-                print('Invalid constraint value for %s: (lb, primal, ub) = (%s, %s, %s)'
-                      % (c.name, str(c.lb), str(c.primal), str(c.ub)))
-                raise OptimizationError('Invalid constraint value for  %s ' % c.name)
+                error = True
         elif c.primal > c.ub + model.tolerance or c.primal < c.lb - model.tolerance:
+            error = True
+        if error:
             print('Invalid constraint value for %s: (lb, primal, ub) = (%s, %s, %s)'
                   % (c.name, str(c.lb), str(c.primal), str(c.ub)))
             raise OptimizationError('Invalid constraint value for  %s ' % c.name)
@@ -197,3 +204,110 @@ def load_reaction_weights(filename, rxn_names='reactions', weight_names='weights
     df.index = df[rxn_names]
     reaction_weights = df[weight_names].to_dict()
     return {str(k): float(v) for k, v in reaction_weights.items() if float(v) == float(v)}
+
+def check_model_primals(model, rw, eps, thr, savename = 'primal_check.csv'):
+    integ = model.solver.configuration.tolerances.integrality
+    feasib = model.solver.configuration.tolerances.feasibility
+    rllimit = integ * np.max([r.bounds for r in model.reactions]) + feasib *2
+    rhlimit = (rllimit + 1e-6) / (1 - rllimit - feasib)
+    if eps<rhlimit:
+        print('epsilon is below rhlimit, there will probably be errors')
+
+    var_primals = model.solver.primal_values
+    # forward = np.empty(len(model.reactions))
+    # reverse = np.empty(len(model.reactions))
+    flux = np.empty(len(model.reactions))
+    x = np.empty(len(model.reactions))
+    xfor = np.empty(len(model.reactions))
+    xrev = np.empty(len(model.reactions))
+    status = {}
+    for i, r in enumerate(model.reactions):
+        rid = r.id
+        flux[i] = var_primals[r.id] - var_primals[r.reverse_id]
+        # forward[i] = var_primals[r.id]
+        # reverse[i] = var_primals[r.reverse_id]
+        if rw[r.id] > 0:
+            try:
+                x[i] = var_primals['x_' + r.id]
+                xfor[i] = var_primals['xf_' + r.id]
+                xrev[i] = var_primals['xr_' + r.id]
+                # print(r.id + ' ok')
+                if flux[i] >= eps - rllimit:
+                    # if reverse[i] > feasib:
+                    #     status[rid] = 'wrong flux: both directions'
+                    if xfor[i] > integ > xrev[i]:
+                        status[rid] = 'correct flux forward'
+                    elif xfor[i] > integ and xrev[i] > 1:
+                        status[rid] = 'incorrect flux forward: both binaries'
+                    elif xrev[i] > integ > xfor[i]:
+                        status[rid] = 'incorrect flux forward: binary reversed'
+                    else:
+                        status[rid] = 'incorrect flux forward: no binary'
+                elif flux[i] <= -eps + rllimit:
+                    # if forward[i] > feasib:
+                    #     status[rid] = 'wrong flux: both directions'
+                    if xfor[i] > integ > xrev[i]:
+                        status[rid] = 'incorrect flux reverse: binary reversed'
+                    elif xfor[i] > integ and xrev[i] > 1:
+                        status[rid] = 'incorrect flux reverse: both binaries'
+                    elif xrev[i] > integ > xfor[i]:
+                        status[rid] = 'correct flux reverse'
+                    else:
+                        status[rid] = 'incorrect flux reverse: no binary'
+                else:
+                    if xfor[i] > integ > xrev[i]:
+                        status[rid] = 'incorrect flux below epsilon: binary forward'
+                    elif xfor[i] > integ and xrev[i] > 1:
+                        status[rid] = 'incorrect flux below epsilon: both binaries'
+                    elif xrev[i] > integ > xfor[i]:
+                        status[rid] = 'incorrect flux below epsilon: binary reverse'
+                    else:
+                        status[rid] = 'correct flux below epsilon'
+            except:
+                x[i] = None
+                xfor[i] = None
+                xrev[i] = None
+                print(r.id + ' RH binary not present?')
+                status[rid] = 'binary not present'
+        elif rw[r.id] < 0:
+            try:
+                x[i] = var_primals['x_' + r.id]
+                # xfor[i] = None
+                # xrev[i] = None
+                xfor[i] = var_primals['xf_' + r.id]
+                xrev[i] = var_primals['xr_' + r.id]
+                # print(r.id + ' ok')
+                # if forward[i] >  0 and reverse[i] > 0:
+                #     status[rid] = 'wrong noflux: both directions'
+                if np.abs(flux[i]) <= rllimit:
+                    if x[i] < integ:
+                        status[rid] = 'correct noflux'
+                    else:
+                        status[rid] = 'incorrect noflux: binary active'
+                else:
+                    if x[i] < integ:
+                        status[rid] = 'incorrect noflux above tolerance'
+                    else:
+                        status[rid] = 'correct noflux above tolerance'
+            except:
+                x[i] = None
+                xfor[i] = None
+                xrev[i] = None
+                print(r.id + ' RL binary not present?')
+                status[rid] = 'binary not present'
+        else:
+            status[rid] = ''
+            x[i] = None
+            xfor[i] = None
+            xrev[i] = None
+        if 'incorrect' in status[rid] or 'wrong' in status[rid]:
+            print(rid, status[rid])
+
+    df = pd.DataFrame([flux, x, xfor, xrev], columns=[r.id for r in model.reactions],
+                      index=['flux', 'x', 'x_forward', 'x_reverse']).T
+    rw = pd.Series(rw)
+    df['rw'] = rw
+    status = pd.Series(status)
+    df['status'] = status
+    df.to_csv(savename, sep=';')
+    return df
