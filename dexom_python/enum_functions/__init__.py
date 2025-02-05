1fbddc228c7fcf486c31fdbb78ecd5dff3be5988
mstingl <maximilian.stingl@inrae.fr>
2025-02-05 15:59:20 +0100

    Add riptide-like imat function

--- dexom_python/enum_functions/__init__.py
+++ dexom_python/enum_functions/__init__.py
@@ -4,3 +4,4 @@ from .icut_functions import icut
 from .maxdist_functions import maxdist
 from .diversity_enum_functions import diversity_enum
 from .permutation_functions import permute_genelabels
+from .riptide_functions import imat_riptide_like
